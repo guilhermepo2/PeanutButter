075aad174a69562d7128898e3f5710ecfe371c68
Guilherme Oliveira <guilherme.061@gmail.com>
2020-04-11 20:13:49 -0400

    adding premake5

--- PeanutButter/src/PeanutButter.h
+++ PeanutButter/src/PeanutButter.h
@@ -1,9 +1,12 @@
 #pragma once
 
-// For use by applications only
+// This Header File is to be included on clients only.
+
+// Core Includes --------------------
 #include "PeanutButter/Core.h"
 #include "PeanutButter/Application.h"
 #include "PeanutButter/Log.h"
+// ----------------------------------
 
 // Entry Point ---------------------
 #include "PeanutButter/EntryPoint.h"
---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "PeanutButter"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-${cfg.architecture}"
+
+project "PeanutButter"
+	location "PeanutButter"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/thirdparty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"PB_PLATFORM_WINDOWS",
+			"PB_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/SandboxGame")
+		}
+
+	filter "configurations:Debug"
+		defines "PB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PB_DIST"
+		optimize "On"
+
+project "SandboxGame"
+	location "SandboxGame"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/thirdparty/spdlog/include",
+		"PeanutButter/src"
+	}
+
+	links 
+	{
+		"PeanutButter"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"PB_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "PB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PB_DIST"
+		optimize "On"
