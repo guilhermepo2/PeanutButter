f49e5738255b7ac654b6e9dcac04ff4978428f0e
Guilherme Oliveira <guilherme.061@gmail.com>
2020-05-28 18:51:39 -0400

    adding random engine

---
+++ PeanutButter/src/PeanutButter/Utils/Random.cpp
@@ -0,0 +1,7 @@
+#include "pbpch.h"
+#include "Random.h"
+
+namespace PeanutButter {
+	std::mt19937 Random::s_RandomEngine;
+	std::uniform_int_distribution<std::mt19937::result_type> Random::s_Distribution;
+}
---
+++ PeanutButter/src/PeanutButter/Utils/Random.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <random>
+
+// Source: https://github.com/TheCherno/OneHourParticleSystem/
+// TODO: Make a random system that actually allows for different implementation and replication of values.
+
+namespace PeanutButter {
+	class Random {
+	public:
+		static void Init() {
+			s_RandomEngine.seed(std::random_device()());
+		}
+
+		static float Float() {
+			return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
+		}
+
+		static int Int() {
+			return s_Distribution(s_RandomEngine);
+		}
+	private:
+		static std::mt19937 s_RandomEngine;
+		static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
+	};
+}
