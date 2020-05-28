9ca23480afc89f9ce401ca50c983825c8fef5484
Guilherme Oliveira <guilherme.061@gmail.com>
2020-05-27 22:39:15 -0400

    adding doxygen file that uploads directly to the documentation repo

---
+++ GenerateAndUploadDocumentation.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+ECHO Generating Documentation...
+:: Set the Doxygen Path here
+SET DOXYGEN_PATH="C:\Program Files\doxygen\bin"
+
+:: Effectively calling doxygen and executing the Doxyfile in the current folder
+call %DOXYGEN_PATH%\doxygen.exe DoxyfilePBDoc
+PAUSE
